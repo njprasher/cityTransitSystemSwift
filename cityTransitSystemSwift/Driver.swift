7ddc55a97cdabf7e0fa655a55d50893f19658d23
Gurwinder Singh <gurwinder958652@gmail.com>
2019-06-21 19:09:12 -0400

    [ Gurwinder ] Variables declared

--- cityTransitSystemSwift/Driver.swift
+++ cityTransitSystemSwift/Driver.swift
@@ -10,5 +10,13 @@ import Foundation
 
 class Driver{
     
+    // declaring variables
+    var driverName : String?
+    var driverEmail: String?
+    var driverPhone: Int?
+    var driverSalary: Double?
+    var driverBirth = Date()
+    var driverAddress: String?
+    
 
 }
