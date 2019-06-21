1673b1d8b949f651b03820e9ee2b14a32e81cc8e
Gurwinder Singh <gurwinder958652@gmail.com>
2019-06-21 19:10:09 -0400

    [ Gurwinder ] Variables Initialised

--- cityTransitSystemSwift/Driver.swift
+++ cityTransitSystemSwift/Driver.swift
@@ -18,5 +18,14 @@ class Driver{
     var driverBirth = Date()
     var driverAddress: String?
     
+    // INITIALSE THE VARIABLES
+    init(driverName: String,driverEmail: String,driverPhone: Int,driverSalary: Double,driverBirth: Date,driverAddress: String ) {
+        self.driverName = driverName
+        self.driverEmail = driverEmail
+        self.driverPhone = driverPhone
+        self.driverSalary = driverSalary
+        self.driverBirth = driverBirth
+        self.driverAddress = driverAddress
+    }
 
 }
