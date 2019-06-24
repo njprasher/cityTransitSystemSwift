48203a70c7484ed8ddded28164975c9a981e3c64
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-24 12:59:07 -0400

    [Neeraj] Added Rider Fullname Attribute which is computed from first and lastname

--- cityTransitSystemSwift/Rider.swift
+++ cityTransitSystemSwift/Rider.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class Rider: Users
 {
-    var name: String?
-    var birth: Date?
+    var firstName: String?
+    var lastName: String?
     var email: String?
     var contact: String?
     var card: Dictionary<String, Double>?
@@ -19,20 +19,24 @@ class Rider: Users
     private static var riderDict=[String:Rider]()
     
     override init() {
-        self.name = String()
-        self.birth = Date()
+        self.firstName = String()
+        self.lastName = String()
         self.email = String()
         self.contact = String()
         self.card = Dictionary<String, Double>()
         super.init()
     }
     
+    var fullName: String // computed variable
+    {
+        return "\(String(describing: self.firstName)) \(String(describing: self.lastName))"
+    }
+    
     override func Display()
     {
         print("---------Rider Details---------")
         super.Display()
-        print("\nRider Name :" , self.name ?? "No Name Given"
-            , "\nRider Birth Date :" , self.birth ?? "No birthdate given"
+        print("\nRider Name :" , self.fullName
             , "\nRider Email :" , self.email ?? "No email given"
             , "\nRider Contact Number :" , self.contact ?? "No contact number given"
             , "\n\n---------Rider's Card Details---------"
